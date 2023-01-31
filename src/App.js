838a923ca710610d726054c07f912c9938df86b2
Matheus <sergio.matheus1425@gmail.com>
2023-01-31 10:19:43 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,35 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>Matheus Alves</code>
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/matheus.o.i"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
+        </a>
+
+
+        <a
+          className="App-link"
+          href="https://github.com/sergiomatheuss"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/sergio-matheus-976826186/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
         </a>
       </header>
     </div>
